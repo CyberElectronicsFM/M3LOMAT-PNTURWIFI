d39162eddab57a454be6a41a9b9562ebea7b510f
hitlerhack313 <59124860+hitlerhack313@users.noreply.github.com>
2020-06-12 21:18:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,38 +3,8 @@ clear
 echo "             +++++++++++++++++      "
 echo "             (+) BY MENA MAGDY      "
 echo "             +++++++++++++++++      "
+
 #############################################
-printf '\033]2;install.sh\a'
-
-G="\033[1;34m[*] \033[0m"
-S="\033[1;32m[+] \033[0m"
-E="\033[1;31m[-] \033[0m"
-
-if [[ $EUID -ne 0 ]]
-then
-   echo -e ""$E"This script must be run as root!"
-   exit
-fi
-
-{
-ASESR="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
-} &> /dev/null
-if [[ "$ASESR" != 0 ]]
-then 
-   echo -e ""$E"No Internet connection!"
-   exit
-fi
-
-sleep 0.5
-echo
-cat banner/banner.txt
-echo
-
-sleep 1
-echo -e ""$G"Installing dependencies..."
-sleep 1
-
-{
 pkg update
 pkg -y install python
 pkg -y install wget
@@ -45,14 +15,6 @@ apt-get -y install python3-pip
 apt-get -y install wget
 apk update
 apk add python3
-apk add py3-pip
-apk add wget
-pacman -Sy
-pacman -S --noconfirm git
-pacman -S --noconfirm python3
-pacman -S --noconfirm python3-pip
-pacman -S --noconfirm wget
-zypper refresh
 zypper install -y git
 zypper install -y python3
 zypper install -y python3-pip
@@ -74,62 +36,6 @@ xbps-install -S
 xbps-install -y git
 xbps-install -y python3
 xbps-install -y python3-pip
-xbps-install -y wget
-} &> /dev/null
-
-if [[ -d ~/phonia ]]
-then
-sleep 0
-else
-cd ~
-{
-git clone https://github.com/entynetproject/phonia.git
-} &> /dev/null
-fi
-
-if [[ -d ~/phonia ]]
-then
-cd ~/phonia
-else
-echo -e ""$E"Installation failed!"
-exit
-fi
-
-{
-rm config.py
-cp config.example.py config.py
-python3 -m pip install setuptools
-python3 -m pip install -r requirements.txt
-} &> /dev/null
-
-{
-wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-sh -c 'tar -x geckodriver -zf geckodriver-*.tar.gz -O > /usr/local/bin/geckodriver'
-chmod +x /usr/local/bin/geckodriver
-else
-if [[ -d /data/data/com.termux ]]
-then
-sh -c 'tar -x geckodriver -zf geckodriver-*.tar.gz -O > /data/data/com.termux/files/usr/bin'
-chmod +x /data/data/com.termux/files/usr/bin/geckodriver
-else
-sh -c 'tar -x geckodriver -zf geckodriver-*.tar.gz -O > /usr/bin/geckodriver'
-chmod +x /usr/bin/geckodriver
-fi
-fi
-} &> /dev/null
-
-{
-cd bin
-cp phonia /usr/local/bin
-chmod +x /usr/local/bin/phonia
-cp phonia /bin
-chmod +x /bin/phonia
-cp phonia /data/data/com.termux/files/usr/bin
-chmod +x /data/data/com.termux/files/usr/bin/phonia
-} &> /dev/null
-
 sleep 1
 echo -e ""$S"Successfully installed!"
 sleep 1
